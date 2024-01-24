840cbb6d23e2a114a35a674f698e4957e0d987dd
Raul Quintero <raul.quintero.cursoinfotec@gmail.com>
2024-01-23 23:18:33 -0800

    add. report-by-month component & fix library jsPDF

--- src/app/contexts/backoffice/reports/report-by-month/report-by-month.component.ts
+++ src/app/contexts/backoffice/reports/report-by-month/report-by-month.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { OrderService } from 'src/app/core/services/models/order.service';
 import { Order } from 'src/app/core/interfaces/order.interface';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-report-by-month',
   templateUrl: './report-by-month.component.html',
@@ -9,6 +10,9 @@ import { Order } from 'src/app/core/interfaces/order.interface';
 })
 export class ReportByMonthComponent implements OnInit{
   orders: Order [] = [];
+  setSpinner: string = 'visually-hidden';
+  @ViewChild('htmlData') htmlData!: ElementRef;
+  @ViewChild('btnPreview') btnPreview!: ElementRef;
 
   constructor(private orderService: OrderService){}
 
@@ -27,6 +31,51 @@ export class ReportByMonthComponent implements OnInit{
       error: error=>{
         console.log('error: ', error);
       }
-    }
-    )};
+    });
+  }
+
+  downloadPDF() {
+    this.setSpinner = 'visually';
+    // Extraemos el
+
+    const DATA: any = this.htmlData.nativeElement;
+    // const DATA: any = document.querySelector('#htmlData');
+    const doc = new jsPDF('p', 'pt', 'letter');
+    const options = {
+      background: 'red',
+      scale: 3
+    };
+    const btn = this.btnPreview.nativeElement;
+      btn!.classList.add('visually-hidden');
+      html2canvas(DATA).then((canvas:any) => {
+        const imgWidth = 208;
+        const pageHeight = 295;
+        const imgHeight = ((canvas.height * imgWidth) / canvas.width);
+        let heightLeft = imgHeight;
+        let position = 10;
+        heightLeft -= pageHeight;
+
+        const doc = new jsPDF('p', 'mm');
+        doc.setProperties({
+          title: "Reporte de Ventas Mensual",
+          });
+        doc.addImage(canvas, 'PNG', 10, position + 5, imgWidth-20, imgHeight, '', 'FAST');
+        while (heightLeft >= 0) {
+          position = heightLeft - imgHeight;
+          doc.addPage();
+          doc.addImage(canvas, 'PNG', 10, position + 5, imgWidth-20, imgHeight, '', 'FAST');
+          heightLeft -= pageHeight;
+        }
+      this.setSpinner = 'visually-hidden';
+
+
+
+
+      window.open(URL.createObjectURL(doc.output("blob")))
+      // doc.save('ReporteMensual.pdf');
+    });
+    btn!.classList.remove('visually-hidden');
+
+  }
+
 }
